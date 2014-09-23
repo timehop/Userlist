d65e44f7e4812a9b3c1245e7c0f6a7a7e0b22334
Chris Trott <chris@twocentstudios.com>
2014-09-22 20:00:28 -0400

    Initial Commit

---
+++ userlist/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  userlist
+//
+//  Created by Chris Trott on 9/22/14.
+//  Copyright (c) 2014 TwoCentStudios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
